f9431e7e81cdf723b0d29fe0499c43ebca95aec0
syeda suman <38942910+syedasuman@users.noreply.github.com>
2018-09-27 17:49:18 +0500

    Add files via upload

---
+++ 2-template-strings.js
@@ -0,0 +1,34 @@
+/*
+
+- First, refactor the code below to use variables. Then,
+- Refactor  to use Template Strings backticks) instead on concatenation
+
+ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+
+ */
+const costOfApple=10;
+console.log(`One apple costs Rs. ${costOfApple}`);
+console.log(`Two apples cost Rs. ${ costOfApple * 2}`);
+console.log(`A dozen apples cost Rs. ${ costOfApple * 12}`);
+console.log(`Six apples cost Rs. ${ costOfApple * 6}`);
+console.log(`Ten apples cost Rs. ${ costOfApple * 10}`);
+const costOfMango=20;
+console.log(`One mango costs Rs. ${costOfMango}`);
+console.log(`Two mangoes cost Rs. ${ costOfMango * 2}`);
+console.log(`A dozen mangoes cost Rs. ${ costOfMango * 12}`);
+console.log(`Six mangoes cost Rs. ${ costOfMango * 6}`);
+console.log(`Ten mangoes cost Rs. ${ costOfMango * 10}`);
+const costOfBanana=15;
+console.log(`One banana costs Rs. ${costOfBanana}`);
+console.log(`Two bananas cost Rs. ${ costOfBanana * 2}`);
+console.log(`A dozen bananas cost Rs. ${ costOfBanana * 12}`);
+console.log(`Six bananas cost Rs. ${ costOfBanana * 6}`);
+console.log(`Ten bananas cost Rs. ${ costOfBanana * 10}`);
+const costOfPeaches=20;
+console.log(`One peach costs Rs. ${costOfPeaches}`);
+console.log(`Two peaches cost Rs. ${ costOfPeaches * 2}`);
+console.log(`A dozen peaches cost Rs. ${ costOfPeaches * 12}`);
+console.log(`Six peaches cost Rs. ${ costOfPeaches* 6}`);
+console.log(`Ten peaches cost Rs. ${ costOfPeaches* 10}`);
+
+console.log(`one banana, one peach, one apple and one mango together cost Rs.  ${ costOfApple + costOfMango + costOfBanana + costOfPeaches}`);
---
+++ 3-functions/3-functions.js
@@ -0,0 +1,64 @@
+let intervalId;
+function actionCorrect(){
+    document.querySelector("header").innerHTML = "CORRECT";
+    document.querySelector("header").classList.toggle("success-text", true);
+    document.querySelector("header").classList.toggle("error-text", false);
+    let counter = 0;
+    clearInterval(intervalId);
+    intervalId = setInterval(function() {
+      let color = "#28a745";
+      if (counter++ % 2) {
+        color = "#1DE9B6";
+      }
+      document.querySelector("body").style["background-color"] = color;
+    }, 500);
+}
+
+function actionIncorrect(){
+   document.querySelector("header").innerHTML = "INCORRECT";
+    document.querySelector("header").classList.toggle("success-text", false);
+    document.querySelector("header").classList.toggle("error-text", true);
+    let counter = 0;
+    clearInterval(intervalId);
+    intervalId = setInterval(function() {
+      let color = "#FF3D00";
+      if (counter++ % 2) {
+        color = "#BF360C";
+      }
+      document.querySelector("body").style["background-color"] = color;
+    }, 500);
+}
+
+function submitMathAns() {
+  const answer = parseInt(document.querySelector("#math input").value);
+  if (answer === 4) {
+    // correct
+  actionCorrect();
+  } else {
+    // incorrect
+   actionIncorrect();
+  }
+}
+
+function submitEnglishAns() {
+  const answer = document.querySelector("#english input[name='webdev']:checked")
+    .value;
+  if (answer === "website") {
+    // correct
+   actionCorrect();
+  } else {
+    // incorrect
+    actionIncorrect();
+  }
+}
+
+function submitScienceAns() {
+  const answer = document.querySelector("#science input").value;
+  if (answer.toLowerCase() === "blue") {
+    // correct
+   actionCorrect();
+  } else {
+    // incorrect
+    actionIncorrect();
+  }
+}
